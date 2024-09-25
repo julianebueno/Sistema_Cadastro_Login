0fc4765f2b2d0912a390d85d6b90688d047dc62c
Juliane Bueno <julianebueno1@gmail.com>
2024-09-25 02:10:56 -0300

    remoção dotenv

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,17 +1,13 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 
-// ERRO DE CONFIGURAÇÃO
-import { config } from "dotenv";
-config();
-
 const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGINGSENDERID,
-  appId: process.env.APPID,
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
 };
 
 if (!firebase.apps.length) {
